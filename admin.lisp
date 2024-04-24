857378a2bfabfd5eb51d453678a31b195c2c6b51
Sjoerd Dost <deosjr@gmail.com>
2024-04-24 12:30:58 +0200

    add codeflask form

--- admin.lisp
+++ admin.lisp
@@ -50,3 +50,92 @@
 
 (handlefunc "/admin" (lambda (w r) (render w admintmpl (routingtable))))
 )
+
+(begin
+(define newadmin (string-append "{{define \"content\"}}
+<div>
+    <p>
+     <input name='path' id='path' type='text' placeholder='Path' value='{{ str .path }}'>
+    <span class='error'>{{ str .patherr }}</span>
+    </p>
+    <p>
+    <div id=editor></div>
+    <span class='error'>{{ str .codeerr }}</span>
+    </p>
+    <input name='output' id='output' type='text'>
+    <button hx-post='/admin/new' hx-include='#path,#output'>Save</button>
+</div>
+
+<p>
+    <a href='/admin'>Back</a>
+</p>
+<style>
+#output {
+    display: none;
+}
+.codeflask.codeflask--has-line-numbers {
+    left: 200px;
+}
+.codeflask.codeflask--has-line-numbers:before {
+    background: var(--gray-12); // from missing.min.css
+}
+/*
+.codeflask .token.keyword {
+    color: purple;
+}
+.codeflask .token.string {
+    color: brown;
+}
+*/
+</style>
+<script type='module'>
+import CodeFlask from 'https://cdn.jsdelivr.net/npm/codeflask@1.4.1/+esm';
+import Prism from 'https://cdn.jsdelivr.net/npm/prismjs@1.29.0/+esm';
+//import 'https://cdn.jsdelivr.net/npm/prismjs@1.29.0/components/prism-lisp.min.js' 
+
+const editor = document.getElementById('editor');
+const output = document.getElementById('output');
+
+const prismGrammarLisp = {
+  string: {
+    pattern: /\"(\\\"|[^\"])+\"/,
+    greedy: true,
+  },
+  paren: {
+    pattern: /[()]/,
+    alias: 'punctuation',
+  },
+  builtin: {
+    pattern: /\b(lambda|define|if|let)\b/,
+    alias: 'keyword',
+  },
+  symbol: {
+    pattern: /[^ ]+/,
+  },
+};
+
+const flask = new CodeFlask(editor, {
+    language: 'lisp',
+    lineNumbers: true,
+    defaultTheme: false
+});
+flask.addLanguage('lisp', prismGrammarLisp);
+
+flask.onUpdate( e => {
+    output.value = e
+})
+
+// disable Prism autohighlighting on load
+window.Prism = window.Prism || {};
+Prism.manual = true;
+
+flask.updateCode(`(lambda (w r) \"implement me\")`)
+console.log(flask.getCode())
+</script>
+
+{{end}}"))
+(define newadmintmpl (template adminlayout newadmin))
+(handlefunc "/admin/new" (lambda (w r)
+    (if (eqv? (request:method r) "GET") (render w newadmintmpl (make-hashmap))
+    (if (eqv? (request:method r) "POST") (httpwrite w "great-success" )))))
+)
